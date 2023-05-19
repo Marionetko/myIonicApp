81f7ef296425497a71cf46fb470d3f55e06b1d64
Marionetko <webius.net@gmail.com>
2023-05-19 17:10:26 +0300

    added intro slider

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,13 @@
 import { IonButton, IonCard, IonCardContent, IonContent, IonHeader, IonIcon, IonInput, IonPage, IonTitle, IonToolbar, useIonRouter } from '@ionic/react';
-import React from 'react';
+import React, { useState } from 'react';
 import { logInOutline, personCircleOutline } from 'ionicons/icons';
 import ION from '../assets/ion.svg';
+import Intro from '../components/Intro';
 
 const Login: React.FC = () => {
 
     const router = useIonRouter();
+    const [introSeen, setIntroSeen] = useState(false);
 
     const doLogin = (event: any) => {
         event.preventDefault();
@@ -13,70 +15,80 @@ const Login: React.FC = () => {
         // router.push('/home', 'root');
     };
 
+    const finishIntro = async() => {
+        console.log('FIN');
+        setIntroSeen(true);
+    }
+
     return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar color={'success'}>
-                    <IonTitle>Ionic React App</IonTitle>
-                </IonToolbar>
-            </IonHeader>
+        <>
+            {!introSeen ? (
+                <Intro onFinish={finishIntro} />
+            ) : (
+            <IonPage>
+                <IonHeader>
+                    <IonToolbar color={'success'}>
+                        <IonTitle>Ionic React App</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
 
-            <IonContent scrollY={false}>
-                <div className="ion-text-center ion-padding">
-                    <img 
-                        src={ION} 
-                        alt="Ion logo" 
-                        width={"50%"}
-                    />
-                </div>
-                <IonCard>
-                    <IonCardContent>
-                        <form onSubmit={doLogin}>
-                            <IonInput 
-                                fill="outline" 
-                                labelPlacement="floating" 
-                                label="Email" 
-                                type="email" 
-                                placeholder="your@email.com"
-                            ></IonInput>
-                            <IonInput 
-                                className="ion-margin-top" 
-                                fill="outline" 
-                                labelPlacement="floating" 
-                                label="Password" 
-                                type="password" 
-                                placeholder="Your password"
-                            ></IonInput>
-                            <IonButton 
-                                className="ion-margin-top" 
-                                type="submit" 
-                                expand="block"
-                            >
-                                Login
-                                <IonIcon 
-                                    icon={logInOutline} 
-                                    slot="end"
-                                ></IonIcon>
-                            </IonButton>
-                            <IonButton 
-                                className="ion-margin-top" 
-                                color={"secondary"} 
-                                type="button" 
-                                expand="block"
-                                routerLink="/register"
-                            >
-                                Create account
-                                <IonIcon 
-                                    icon={personCircleOutline} 
-                                    slot="end"
-                                ></IonIcon>
-                            </IonButton>
-                        </form>
-                    </IonCardContent>
-                </IonCard>
-            </IonContent>
-
-        </IonPage>
+                <IonContent scrollY={false}>
+                    <div className="ion-text-center ion-padding">
+                        <img 
+                            src={ION} 
+                            alt="Ion logo" 
+                            width={"50%"}
+                        />
+                    </div>
+                    <IonCard>
+                        <IonCardContent>
+                            <form onSubmit={doLogin}>
+                                <IonInput 
+                                    fill="outline" 
+                                    labelPlacement="floating" 
+                                    label="Email" 
+                                    type="email" 
+                                    placeholder="your@email.com"
+                                ></IonInput>
+                                <IonInput 
+                                    className="ion-margin-top" 
+                                    fill="outline" 
+                                    labelPlacement="floating" 
+                                    label="Password" 
+                                    type="password" 
+                                    placeholder="Your password"
+                                ></IonInput>
+                                <IonButton 
+                                    className="ion-margin-top" 
+                                    type="submit" 
+                                    expand="block"
+                                >
+                                    Login
+                                    <IonIcon 
+                                        icon={logInOutline} 
+                                        slot="end"
+                                    ></IonIcon>
+                                </IonButton>
+                                <IonButton 
+                                    className="ion-margin-top" 
+                                    color={"secondary"} 
+                                    type="button" 
+                                    expand="block"
+                                    routerLink="/register"
+                                >
+                                    Create account
+                                    <IonIcon 
+                                        icon={personCircleOutline} 
+                                        slot="end"
+                                    ></IonIcon>
+                                </IonButton>
+                            </form>
+                        </IonCardContent>
+                    </IonCard>
+                </IonContent>
+            </IonPage>
+            )}
+        </>
     );
 };
 
